8e34a7961209041bf74500cef145456e7506bab0
Christopher Bale <christopherbale@icloud.com>
2020-01-27 08:55:07 +0000

    Cleaned up syntax in restart.sh

--- restart.sh
+++ restart.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 STATUS=`systemctl is-active php7.2-fpm.service`
-  if [[ ${STATUS} == 'active' ]]; then
-   :
-  else 
-    systemctl restart php7.2-fpm.service 
-  fi
+
+if [[ ${STATUS} == 'active' ]]; then
+ :
+else 
+ systemctl restart php7.2-fpm.service 
+fi
